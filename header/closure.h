ea68d3eb4e1a11f8af902708fca7fd5834157a53
Plankp T <plankp@outlook.com>
2016-12-22 10:18:25 -0500

    Added missing license header

--- header/closure.h
+++ header/closure.h
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (c) 2016 Paul Teng
+ * 
+ * PCLIB is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU Lesser General Public License as   
+ * published by the Free Software Foundation, version 3.
+ *
+ * PCLIB is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __CLOSURE_H__
 #define __CLOSURE_H__
 

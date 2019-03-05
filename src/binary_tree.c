34be6b165269118135b57d2485daea6fbe13be01
Paul Teng <plankp@outlook.com>
2019-03-05 18:38:30 -0500

    Simplify code

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -49,6 +49,27 @@ void traversal_inorder
   traversal_inorder(node->rhs, has_alloc_value, it);
 }
 
+static
+tree_node ** find_tree_node
+(binary_tree const * restrict const tree, void const * restrict const key)
+{
+  /* this is more like a multimap meaning you can have
+   * multiple nodes with the same key (compare --> 0)
+   */
+  tree_node * const * node = &tree->root;
+  while (*node != NULL)
+  {
+    int const cmp = tree->key_compare((*node)->key, key);
+    if (cmp > 0) node = &((*node)->rhs);
+    else if (cmp < 0) node = &((*node)->lhs);
+    else /* cmp == 0 */
+    {
+      break;
+    }
+  }
+  return (tree_node **) node;
+}
+
 bool init_bintree
 (binary_tree * const tree, key_cmp * key_compare, size_t data_size)
 {
@@ -82,62 +103,41 @@ void bintree_clear
 bool bintree_put
 (binary_tree * restrict const tree, void const * restrict key, void const * restrict value)
 {
-  /* this is more like a multimap meaning you can have
-   * multiple nodes with the same key (compare --> 0)
-   */
-  tree_node ** node = &tree->root;
-  while (*node != NULL)
+  tree_node ** node = find_tree_node(tree, key);
+  if (*node == NULL)
   {
-    int const cmp = tree->key_compare((*node)->key, key);
-    if (cmp > 0) node = &((*node)->rhs);
-    else if (cmp < 0) node = &((*node)->lhs);
-    else /* cmp == 0 */
-    {
-      /* resize the current node */
-      tree_node * current = *node;
-      tree_node * resized = tree->blk == 0 ? current : realloc(current, calc_node_size(tree, current->count + 1));
-      if (resized == NULL) return false;
+    /* create a blank node */
+    tree_node *new_node = malloc(calc_node_size(tree, 1));
+    if (new_node == NULL) return NULL;
 
-      ++tree->len;
-      memcpy(resized->values + tree->blk * resized->count++, value, tree->blk);
-      *node = resized;
-      return true;
-    }
+    new_node->count = 1;
+    new_node->key = key;
+    new_node->lhs = NULL;
+    new_node->rhs = NULL;
+    memcpy(new_node->values, value, tree->blk);
+
+    ++tree->len;
+    *node = new_node;
+    return true;
   }
 
-  /* *node == NULL, which means creating a blank node */
-  tree_node *new_node = malloc(calc_node_size(tree, 1));
-  if (new_node == NULL) return NULL;
-
-  new_node->count = 1;
-  new_node->key = key;
-  new_node->lhs = NULL;
-  new_node->rhs = NULL;
-  memcpy(new_node->values, value, tree->blk);
+  /* resize the current node */
+  tree_node * current = *node;
+  tree_node * resized = tree->blk == 0 ? current : realloc(current, calc_node_size(tree, current->count + 1));
+  if (resized == NULL) return false;
 
   ++tree->len;
-  *node = new_node;
+  memcpy(resized->values + tree->blk * resized->count++, value, tree->blk);
+  *node = resized;
   return true;
 }
 
 bool bintree_put_if_absent
 (binary_tree * restrict const tree, void const * restrict key, void const * restrict value)
 {
-  /* this is more like a multimap meaning you can have
-   * multiple nodes with the same key (compare --> 0)
-   */
-  tree_node ** node = &tree->root;
-  while (*node != NULL)
-  {
-    int const cmp = tree->key_compare((*node)->key, key);
-    if (cmp > 0) node = &((*node)->rhs);
-    else if (cmp < 0) node = &((*node)->lhs);
-    else /* cmp == 0 */
-    {
-      /* key is not absent, do not insert */
-      return false;
-    }
-  }
+  tree_node ** node = find_tree_node(tree, key);
+
+  if (*node != NULL) return false;
 
   /* *node == NULL, which means creating a blank node */
   tree_node *new_node = malloc(calc_node_size(tree, 1));
@@ -157,83 +157,74 @@ bool bintree_put_if_absent
 bool bintree_remove
 (binary_tree * restrict const tree, void const * restrict key)
 {
-  tree_node ** node = &tree->root;
-  while (*node != NULL)
+  tree_node ** node = find_tree_node(tree, key);
+
+  if (*node == NULL) return false;
+
+  tree_node * current = *node;
+  tree->len -= current->count;
+
+  if (current->lhs == NULL)
   {
-    int const cmp = tree->key_compare((*node)->key, key);
-    if (cmp > 0) node = &((*node)->rhs);
-    else if (cmp < 0) node = &((*node)->lhs);
-    else /* cmp == 0 */
-    {
-      tree_node * current = *node;
-      tree->len -= current->count;
-
-      if (current->lhs == NULL)
-      {
-        /* pull rhs up to replace the current node */
-        *node = current->rhs;
-        /* make sure the pulled rhs does not get freed */
-        current->rhs = NULL;
-        free_subsequent_nodes(current);
-        return true;
-      }
-
-      if (current->rhs == NULL)
-      {
-        /* pull lhs up to replace the current node */
-        *node = current->lhs;
-        /* make sure the pulled lhs does not get freed */
-        current->lhs = NULL;
-        free_subsequent_nodes(current);
-        return true;
-      }
-
-      /* node contains both children: pull up the minimum node of rhs */
-      tree_node *parent = current;
-      tree_node *min = current->rhs;
-      while (min->lhs != NULL)
-      {
-        parent = min;
-        min = min->lhs;
-      }
-
-      if (parent == current)
-      {
-        /* parent           parent
-         *  /  \             /  \
-         * ??? min   -->   ???  min'
-         *       \
-         *       min'
-         */
-        parent->rhs = min->rhs;
-      }
-      else
-      {
-        /* parent           parent
-         *  /  \             /  \
-         * min ???   -->   min' ???
-         *   \
-         *   min'
-         */
-        parent->lhs = min->rhs;
-      }
-
-      /* swap out the current node (being removed) and fix links */
-      *node = min;
-      min->lhs = current->lhs;
-      min->rhs = current->rhs;
-
-      /* detach removed node's connections and clean it up */
-      current->lhs = NULL;
-      current->rhs = NULL;
-      free_subsequent_nodes(current);
-
-      return true;
-    }
+    /* pull rhs up to replace the current node */
+    *node = current->rhs;
+    /* make sure the pulled rhs does not get freed */
+    current->rhs = NULL;
+    free_subsequent_nodes(current);
+    return true;
   }
 
-  /* *node == NULL, which means no matches */
-  return false;
+  if (current->rhs == NULL)
+  {
+    /* pull lhs up to replace the current node */
+    *node = current->lhs;
+    /* make sure the pulled lhs does not get freed */
+    current->lhs = NULL;
+    free_subsequent_nodes(current);
+    return true;
+  }
+
+  /* node contains both children: pull up the minimum node of rhs */
+  tree_node *parent = current;
+  tree_node *min = current->rhs;
+  while (min->lhs != NULL)
+  {
+    parent = min;
+    min = min->lhs;
+  }
+
+  if (parent == current)
+  {
+    /* parent           parent
+      *  /  \             /  \
+      * ??? min   -->   ???  min'
+      *       \
+      *       min'
+      */
+    parent->rhs = min->rhs;
+  }
+  else
+  {
+    /* parent           parent
+      *  /  \             /  \
+      * min ???   -->   min' ???
+      *   \
+      *   min'
+      */
+    parent->lhs = min->rhs;
+  }
+
+  /* swap out the current node (being removed) and fix links */
+  *node = min;
+  min->lhs = current->lhs;
+  min->rhs = current->rhs;
+
+  /* detach removed node's connections and clean it up */
+  current->lhs = NULL;
+  current->rhs = NULL;
+  free_subsequent_nodes(current);
+
+  return true;
 }
 
 bool bintree_has_key
@@ -245,39 +236,19 @@ bool bintree_has_key
 size_t bintree_count_matches
 (binary_tree const * restrict const tree, void const * restrict key)
 {
-  tree_node * const * node = &tree->root;
-  while (*node != NULL)
-  {
-    int const cmp = tree->key_compare((*node)->key, key);
-    if (cmp > 0) node = &((*node)->rhs);
-    else if (cmp < 0) node = &((*node)->lhs);
-    else /* cmp == 0 */
-    {
-      return (*node)->count;
-    }
-  }
-
-  /* *node == NULL, which means no matches */
-  return 0;
+  tree_node ** node = find_tree_node(tree, key);
+  return *node == NULL ? 0 : (*node)->count;
 }
 
 void const * bintree_get
 (binary_tree const * restrict const tree, void const * restrict key, size_t * restrict matches)
 {
-  tree_node * const * node = &tree->root;
-  while (*node != NULL)
-  {
-    int const cmp = tree->key_compare((*node)->key, key);
-    if (cmp > 0) node = &((*node)->rhs);
-    else if (cmp < 0) node = &((*node)->lhs);
-    else /* cmp == 0 */
-    {
-      if (matches != NULL) *matches = (*node)->count;
-      return tree->blk > 0 ? (*node)->values : NULL;
-    }
-  }
+  tree_node ** node = find_tree_node(tree, key);
 
-  return NULL;
+  if (*node == NULL) return NULL;
+
+  if (matches != NULL) *matches = (*node)->count;
+  return tree->blk > 0 ? (*node)->values : NULL;
 }
 
 void const * bintree_get_or_default

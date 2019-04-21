2e3777e9a70548bf10a4455cf5d1483009c2a5c8
Paul Teng <plankp@outlook.com>
2019-04-21 11:50:55 -0400

    Document ring buffer type

--- header/ring_buffer.h
+++ header/ring_buffer.h
@@ -29,18 +29,94 @@ typedef struct ring_buffer
   char * end;
 } ring_buffer;
 
+/**
+ * Initializes a ring buffer with the specified capacity
+ *
+ * @param buf - Pointer to an uninitialized ring buffer
+ * @param cnt - Capacity
+ * @param data_size - Size of each element
+ *
+ * @return true if cnt and data_size are more than zero
+ */
 bool init_ringbuf         (ring_buffer * buf, size_t cnt, size_t data_size);
+
+/**
+ * Frees a ring buffer, making it the same as uninitialized
+ *
+ * @param buf - Pointer to an initialized ring buffer
+ */
 void free_ringbuf         (ring_buffer * buf);
+
+/**
+ * Clears the ring buffer
+ *
+ * @param buf - Pointer to an initialized ring buffer
+ */
 void ringbuf_clear        (ring_buffer * buf);
 
+/**
+ * Returns the size of the ring buffer
+ *
+ * @param buf - Pointer to initialized ring buffer
+ *
+ * @return size of the ring buffer
+ */
 size_t ringbuf_size       (const ring_buffer * buf);
+
+/**
+ * Returns the capacity of the ring buffer
+ *
+ * @param buf - Pointer to initialized ring buffer
+ *
+ * @return capacity of the ring buffer
+ */
 size_t ringbuf_capacity   (const ring_buffer * buf);
+
+/**
+ * Checks if ring buffer is empty
+ *
+ * @param buf - Pointer to initialized ring buffer
+ *
+ * @return true if ring buffer is empty
+ */
 bool ringbuf_empty        (const ring_buffer * buf);
 
+/**
+ * Tries to append an element to the logical end of the ring buffer.
+ *
+ * @param buf - Pointer to initialized ring buffer
+ * @param data - Pointer to element being added
+ *
+ * @return true if element was successfully added
+ */
 bool ringbuf_offer        (ring_buffer *restrict buf, const void *restrict data);
+
+/**
+ * Tries to remove an element from the logical head of the ring buffer.
+ *
+ * @param buf - Pointer to initialized ring buffer
+ * @param out - Pointer that will be filled with the removed value; ignored if
+ *              NULL
+ *
+ * @return true if buffer was not empty and element was successfully removed
+ */
 bool ringbuf_poll         (ring_buffer *restrict buf, void *restrict out);
+
+/**
+ * Returns a pointer to the next element to be polled.
+ *
+ * @param buf - Pointer to initialized ring buffer
+ *
+ * @return NULL if buffer is empty
+ */
 void const *ringbuf_peek  (const ring_buffer * buf);
 
+/**
+ * Iterates through the ring buffer in order of insertion
+ *
+ * @param buf - Pointer to initialized ring buffer
+ * @param it - An action to be performed on each pair
+ */
 void ringbuf_foreach      (const ring_buffer * buf, void (*it)(const void *));
 
 #endif
